a8caefdef08e0a1bc6cfd40fbe866eb9d1c83af9
许润泽 <xurunze@xurunzedeMacBook-Pro.local>
2018-05-02 17:14:59 +0800

    up

--- exec.sh
+++ exec.sh
@@ -1,1 +1,4 @@
-python cluster_test.py --method DBSCAN --videoDir /workspace/data/blue/feature/data_blue_feature --picDir /workspace/data/blue/crop_faces --saveResult True --saveDir result
+for((i=0.31;i<=0.32;i+=0.01));  
+do   
+python cluster_test.py --method DBSCAN --videoDir /workspace/data/blue/feature/data_blue_feature --picDir /workspace/data/blue/crop_faces --saveResult True --saveDir result --eps $i;
+done 
