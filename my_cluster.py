e6b7171d39b5e8b862df1e8242a36b9539341ae1
许润泽 <xurunze@xurunzedeMacBook-Pro.local>
2018-05-03 15:04:47 +0800

    up

--- my_cluster.py
+++ my_cluster.py
@@ -50,8 +50,9 @@ def feature_data_reader_fromList(filePathList):
     #Use first one to initialize
     feature_list = np.load(filePathList[0])
     #Concat else
-    cnt = 0
-    for fileFullPath in filePathList[1:]:    
+    cnt = -1
+    noHeadFilePathList = filePathList[1:]
+    for fileFullPath in noHeadFilePathList:    
         cnt += 1
         if cnt%1000 == 0:
             print "Process", name, "done concating", cnt
@@ -59,6 +60,8 @@ def feature_data_reader_fromList(filePathList):
         try:
             feature_list = np.vstack((feature_list, featureVec))
         except:
+            noHeadFilePathList.pop(cnt)
+            cnt -= 1
             print feature_list.shape, featureVec.shape, fileFullPath
     return np.asarray(feature_list)
 
@@ -134,17 +137,41 @@ def __compute_pairwise_distance(face_feature_list):
     dist_matrix = 1 - np.dot(face_feature_list, face_feature_list.T)
     return dist_matrix
 
-def my_cluster(videoDir, featureList, picDir, method, saveResult=False, saveDir='result', eps=0.5, nProcess=1, **kwargs):
+def my_cluster(videoDir, featureList, picDir, method, saveResult=False, saveDir='result', eps=0.5, nReaderProcess=1, nClusterProcess=1, **kwargs):
 
     resultDict = {}
     t0 = time.time()
     print "Start loading data: ", t0
     #feature_list, global_pic, filePathList = feature_data_reader(videoDir, featureList)
-    feature_list, global_pic, filePathList = multiprocess_feature_data_reader(videoDir, featureList, nProcess)
+    feature_list, global_pic, filePathList = multiprocess_feature_data_reader(videoDir, featureList, nReaderProcess)
 
     t1 = time.time()
     print "Done loading data. Start clustering: ", t1, "Loading data time cost: ", t1 - t0
 
+    # pool = multiprocessing.Pool(nClusterProcess)
+    # pos = 0
+    # total = 
+    # step = 
+
+    # pos = 0
+    #     step = total_line / nProcess + 1
+    #     resList = []
+    #     for i in range(nProcess):
+    #         if i == nProcess - 1:
+    #             resList.append(p.apply_async(feature_data_reader_fromList,args=(filePathList[pos:],)))
+    #         else: 
+    #             resList.append(p.apply_async(feature_data_reader_fromList,args=(filePathList[pos:pos+step],)))
+    #             pos += step
+    #     p.close()
+    #     p.join()
+    #     for i in range(nProcess):
+    #         if i == 0:  
+    #             feature_list = resList[i].get()
+    #         else:
+    #             feature_list = np.vstack((feature_list, resList[i].get()))
+
+    my_cluster_after_read(feature_list, filePathList, picDir, method, saveResult, saveDir, eps)
+    '''
     if method == 'RankOrder':
         if (kwargs.has_key('RO_n_neighbors')) and (kwargs.has_key('RO_thresh')):
             y_pred = rankOrder_cluster_format(feature_list, kwargs['RO_n_neighbors'], kwargs['RO_thresh'])
@@ -173,16 +200,43 @@ def my_cluster(videoDir, featureList, picDir, method, saveResult=False, saveDir=
             
             shutil.copyfile(picPath, classDir+picName)
         #shutil.copytree(saveDirPrefix, os.path.join(videoDir, saveDirPrefix))
+    '''
     
+
+    # assert len(y_pred) == len(filePathList)
+    # for i in range(len(y_pred)):
+    #     resultDict[filePathList[i].split('/')[-1].replace('.npy', '')] = y_pred[i]
+    return #resultDict
+
+def my_cluster_after_read(feature_list, filePathList, picDir, method, saveResult=False, saveDir='result', eps=0.5):
+    t1 = time.time()
+    y_pred = cluster_face_features(feature_list=feature_list, method=method, eps=eps)
+
+    t2 = time.time()
+    print "Done clustering. Start copying result: ", t2, "Clustering time cost", t2 - t1
+
+    if saveResult:
+        #saveDirPrefix = 'result_' + method + videoDir.replace('./', '')
+        saveDirPrefix = saveDir
+        for i in range(len(y_pred)):
+            classDir = saveDirPrefix+'/'+str(y_pred[i])+'/'
+            try:
+                os.makedirs(classDir)
+            except:
+                pass
+            picName = filePathList[i].replace('.npy', '.jpg').split('/')[-1]
+            if picName.startswith('/'):
+                picName = picName[1:]
+            picPath = os.path.join(picDir, picName)
+            
+            shutil.copyfile(picPath, classDir+picName)
     t3 = time.time()
     print "Done copying: ", t3, "Copying time cost", t3 - t2
     print "Exiting..."
     exit(0)
 
-    # assert len(y_pred) == len(filePathList)
-    # for i in range(len(y_pred)):
-    #     resultDict[filePathList[i].split('/')[-1].replace('.npy', '')] = y_pred[i]
-    return resultDict
+    return
+
 
 def cluster_from_video_dir(videoDir, featureList, picDir, methodList=['DBSCAN'], saveResult=False, saveDir='result', eps=0.5, nProcess=1):
     methodResultDict = {}
