bb45050df501289c52068170418e9c54f162f4fa
许润泽 <xurunze@xurunzedeMacBook-Pro.local>
2018-05-09 14:47:38 +0800

    up

--- my_cluster.py
+++ my_cluster.py
@@ -206,7 +206,7 @@ def cluster_from_video_dir(videoDir, featureList, picDir, methodList=['DBSCAN'],
     print "Done loading data. Start clustering: ", t1, "Loading data time cost: ", t1 - t0
     
     method = methodList[0]
-    if type(eps) == int:
+    if type(eps) != dict:
         eps = [eps]
     epsResultDict = {}
     for paraEps in eps:
