97744228c0e6ccf2f75725fb85e47b85240a6d49
许润泽 <xurunze@xurunzedeMacBook-Pro.local>
2018-05-09 16:34:00 +0800

    up

--- my_cluster.py
+++ my_cluster.py
@@ -170,7 +170,6 @@ def my_cluster_after_read(feature_list, filePathList, picDir, method, saveResult
         #saveDirPrefix = 'result_' + method + videoDir.replace('./', '')
         print "Saving..."
         saveDirPrefix = saveDir
-        print saveDirPrefix
         for i in range(len(y_pred)):
             #classDir = saveDirPrefix+'/'+str(y_pred[i])+'/'  #for fomer
             classDir = saveDirPrefix+'/'+str(y_pred[i])+'/'#+filePathList[i].replace('.jpg_feat.bin', '.jpg').split('/')[-2] + '/'
@@ -187,7 +186,7 @@ def my_cluster_after_read(feature_list, filePathList, picDir, method, saveResult
             if picName.startswith('/'):
                 picName = picName[1:]
             picPath = os.path.join(picDir, picName)
-            shutil.copyfile(picPath, classDir+picName)
+            shutil.copyfile(picPath, classDir+picName.split['/'][-1])
     t3 = time.time()
     print "Done copying: ", t3, "Copying time cost", t3 - t2
     
